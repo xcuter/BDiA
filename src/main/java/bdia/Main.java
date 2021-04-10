51aa1e2654b6d96c50bb2d8253a28ef0a3614147
Xecuter <patrykgr92@gmail.com>
2021-04-10 12:15:54 +0200

    Change Driver Manager to HikkariCP -- first commit

--- src/main/java/bdia/Main.java
+++ src/main/java/bdia/Main.java
@@ -1,22 +1,33 @@
 package bdia;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.*;
 
 public class Main {
 
-    static {
-        try{
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+
+    static{
+        config.setJdbcUrl("jdbc:postgresql://localhost/maas" );
+        config.setUsername( "jee" );
+        config.setPassword( "jee" );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        ds = new HikariDataSource( config );
     }
+
 
     public static void main(String... args) throws SQLException {
 
-        try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/maas","jee","jee")){
+        try(Connection conn = ds.getConnection()){
             try(Statement stmt = conn.createStatement()){
-                try(ResultSet rs = stmt.executeQuery("select * from profiles where ...")){
+                try(ResultSet rs = stmt.executeQuery("select * from profiles")){
                     while (rs.next()){
                         System.out.println(rs.getString("email")+" "+rs.getString("first_name"));
                     }
